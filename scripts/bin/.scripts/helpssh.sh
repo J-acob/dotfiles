e20f9eb11f639bfc10f6cb2f5a21a766556f3ef6
jacobx10@gmail.com <wsl-ubuntu@JACOB-PC.localdomain>
2022-05-05 13:51:23 -0700

    Fixed the ssh script...? I think?

--- scripts/bin/.scripts/helpssh.sh
+++ scripts/bin/.scripts/helpssh.sh
@@ -1,2 +1,14 @@
-eval `ssh-agent -s`
-echo "run ssh-add <UR-PRIVATE KEY GOES HERE>"
+ssh_key=$1
+
+
+if [[ -n "$ssh_key" ]]; then
+  eval `ssh-agent -s`
+  ssh-add $1 
+else
+  echo "You need to give me a ssh key bro" 
+fi
+
+
+
+
+
